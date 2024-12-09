473ff6eae4fb09c2be2a80610def84fc25c2f2de
Berlian Ika Isabela <beerlnkaaisbella@gmail.com>
2024-12-09 16:36:57 +0700

    update lagiii

--- coba/coba.c
+++ coba/coba.c
@@ -1,5 +1,16 @@
+<<<<<<< Updated upstream
+=======
+#include <iostream>
+>>>>>>> Stashed changes
 #include <stdio.h>
 
 int main(){
+    int n;
+
+    cout<<"masukkan jumlah baris: ";
     
+<<<<<<< Updated upstream
 }
+=======
+}
+>>>>>>> Stashed changes
